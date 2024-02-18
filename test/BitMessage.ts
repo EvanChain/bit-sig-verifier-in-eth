c34e141dc45fbc646f55f1a6ed0f76ed1197dc5f
evan <evan@hotfi.com>
2024-02-18 11:49:17 +0800

    verify signature

--- test/BitMessage.ts
+++ test/BitMessage.ts
@@ -5,6 +5,8 @@ import {
 import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs"
 import { expect } from "chai"
 import { ethers } from "hardhat"
+import { Message, PrivateKey, crypto } from 'bitcore-lib'
+import * as elliptic from 'elliptic'
 
 describe("BitMessage", function () {
 
@@ -13,15 +15,62 @@ describe("BitMessage", function () {
     const BitMessage = await ethers.getContractFactory("BitMessage")
     const bitmessage = await BitMessage.deploy()
 
-    return { bitmessage }
+    const [signer] = await ethers.getSigners()
+    
+    //generate random bit signer
+    const curve = new elliptic.ec('secp256k1')
+
+    const secret = curve.genKeyPair().getPrivate().toString('hex')
+
+    const bitkey = new PrivateKey(secret)
+    
+    const ethwallet = new ethers.Wallet(secret)
+
+    return { bitmessage, signer, bitkey, ethwallet }
+  }
+
+  function toBeHex(data: Buffer) : string {
+    return ethers.toBeHex(BigInt('0x' + data.toString('hex')), 32)
   }
 
   describe("Message Digest", function () {
     it("Should return the right bit message digest", async function () {
       const { bitmessage } = await loadFixture(deployOneYearLockFixture)
-      const digest = await bitmessage.messageDigest("0xc8ee0d506e864589b799a645ddb88b08f5d39e8049f9f702b3b61fa15e55fc73")
+      const message = 'Hello, World!'
+      const ethDigest = ethers.hashMessage(message)
+      console.log("ethDigest:", ethDigest)
+      const bitDigest = (new Message(ethDigest.substring(2))).magicHash().toString('hex')
+      console.log("bitDigest:", bitDigest)
+
+      const digest = await bitmessage.messageDigest(ethDigest)
       console.log('digest:', digest)
-      expect(digest).to.equal("0x65e18fc52d1c8f18497e118c8db284fbbbb906c528eb271be9cf572826fdfdcc")
+      expect(digest.substring(2)).to.equal(bitDigest)
+    })
+
+  })
+
+  describe("Verify Signature", function () {
+    it("Should return the right signer address", async function () {
+      const { bitmessage, signer, bitkey, ethwallet } = await loadFixture(deployOneYearLockFixture)
+      const message = 'Hello, World!'
+      const ethDigest = ethers.hashMessage(message)
+      const bit_message = new Message(ethDigest.substring(2))
+
+      const signature = bit_message.sign(bitkey)
+      console.log('bit signature:', signature)
+
+      const s = crypto.Signature.fromCompact(Buffer.from(signature, 'base64'))
+      // convert to eth signature
+      const convertedSign = ethers.Signature.from({
+        r: toBeHex(s.r.toBuffer()),
+        s: toBeHex(s.s.toBuffer()),
+        v: (s.i ? 0x1c: 0x1b)
+      })
+      console.log('eth signature:', convertedSign.serialized)
+      console.log('signer address:',await ethwallet.getAddress())
+      const recoveredAddress = await bitmessage.recover(ethDigest, convertedSign.serialized)
+      console.log('recovered address:', recoveredAddress)
+      expect(recoveredAddress).to.equal(await ethwallet.getAddress())
     })
 
   })
